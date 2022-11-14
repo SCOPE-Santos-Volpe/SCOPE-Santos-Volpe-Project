eac8145cd77b54c93cee9425c6d858a7607eb7a6
Lilo Heinrich <lheinrich@olin.edu>
2022-11-13 20:05:51 -0500

    push latest code (lilo) incldl. mpo boudaries by state

---
+++ leaflet/heatmap/sales.js
@@ -0,0 +1,39 @@
+// import HeatmapOverlay from 'leaflet-heatmap/leaflet-heatmap.js'
+
+// Create the base Leaflet layer (the map itself)
+
+let baseLayer = L.tileLayer(
+    'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: 'Map data © <a href="https://openstreetmap.org">OpenStreetMap</a>'
+    }
+)
+
+// Configure and create the heatmap.js layer. Check out the heatmap.js Leaflet plugin docs for additional configuration options.
+
+let cfg = {
+    "radius": 40,
+    "useLocalExtrema": true,
+    valueField: 'price'
+};
+let heatmapLayer = new HeatmapOverlay(cfg);
+
+// Determine min/max (from JSON data exposed as variable in sales.js) for the heatmap.js plugin
+
+let min = Math.min(...sales.map(sale => sale.value))
+let max = Math.max(...sales.map(sale => sale.value))
+
+// Create the overall Leaflet map using the two layers we created
+
+let propertyHeatMap = new L.Map('map', {
+    center: new L.LatLng(39.275, -76.613),
+    zoom: 15,
+    layers: [baseLayer, heatmapLayer]
+})
+
+// Add data (from JSON data exposed as variable in sales.js) to the heatmap.js layer
+
+heatmapLayer.setData({
+    min: min,
+    max: max,
+    data: sales
+});
